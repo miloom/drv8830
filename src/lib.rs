b0a47ba8ba050cea67ae71f7ab6922d7e2610879
Mikk Loomets <miloom@taltech.ee>
2024-11-26 19:30:38 +0200

    Unpin version of arduino-hal

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,33 @@
+#![no_std]
 #[cfg(all(feature = "rpi", feature = "embedded"))]
-compile_error!("feature \"foo\" and feature \"bar\" cannot be enabled at the same time");
+compile_error!("feature \"rpi\" and feature \"embedded\" cannot be enabled at the same time");
+#[cfg(all(feature = "embedded", feature = "std"))]
+compile_error!("feature \"embedded\" and feature \"std\" cannot be enabled at the same time");
+
+#[cfg(feature = "std")]
+extern crate std;
+#[cfg(feature = "embedded")]
+use arduino_hal::I2c;
+#[cfg(feature = "embedded")]
+use arduino_hal::prelude::*;
 
 #[cfg(feature = "embedded")]
-use embedded_hal::i2c::Error;
-#[cfg(feature = "embedded")]
-use embedded_hal::i2c::I2c;
+use core::prelude::rust_2021::*;
 #[cfg(feature = "rpi")]
 use rppal::i2c::Error;
 #[cfg(feature = "rpi")]
 use rppal::i2c::I2c;
 
+#[cfg(feature = "rpi")]
 pub type Result<T> = std::result::Result<T, Error>;
+#[cfg(feature = "embedded")]
+pub type Result<T> = core::result::Result<T, arduino_hal::i2c::Error>;
 
 pub trait WriteRegister {
     #[cfg(feature = "rpi")]
     fn write(&self, i2c: &mut I2c) -> Result<()>;
     #[cfg(feature = "embedded")]
-    fn write<T: I2c>(&self, i2c: &mut T) -> Result<()>;
+    fn write(&self, i2c: &mut I2c, chip_addr: u8) -> Result<()>;
 }
 pub trait ReadRegister {
     #[cfg(feature = "rpi")]
@@ -25,16 +36,18 @@ pub trait ReadRegister {
         Self: Sized;
 
     #[cfg(feature = "embedded")]
-    fn new<T: I2c>(i2c: &mut T) -> Result<Self>
+    fn new(i2c: &mut I2c, chip_addr: u8) -> Result<Self>
     where
         Self: Sized;
 }
 
+#[derive(Debug, Default)]
 pub struct Control {
     in1: bool,
     in2: bool,
     // Output voltage in volts that the driver will attempt to match (1.29V - 5.06V)
     vout: f32,
+    pub speed_mult: f32,
 }
 impl Control {
     const ADDRESS: u8 = 0x00;
@@ -42,21 +55,25 @@ impl Control {
         in1: false,
         in2: false,
         vout: 3.2,
+        speed_mult: 1.0,
     };
     pub const REVERSE: Self = Self {
         in1: false,
         in2: true,
         vout: 3.2,
+        speed_mult: 1.0,
     };
     pub const FORWARD: Self = Self {
         in1: true,
         in2: false,
         vout: 3.2,
+        speed_mult: 1.0,
     };
     pub const BRAKE: Self = Self {
         in1: true,
         in2: true,
         vout: 3.2,
+        speed_mult: 1.0,
     };
 }
 impl WriteRegister for Control {
@@ -70,29 +87,29 @@ impl WriteRegister for Control {
     }
 
     #[cfg(feature = "embedded")]
-    fn write<T: I2c>(&self, i2c: &mut T) -> Result<()> {
+    fn write(&self, i2c: &mut I2c, chip_addr: u8) -> Result<()> {
         // VOUT = 4 x VREF x (VSET +1) / 64, where VREF is the internal 1.285-V
         let voltage_enc = (self.vout.clamp(1.29, 5.06) / 0.0803) as u8;
         let write_reg = (voltage_enc << 2) | (u8::from(self.in2) << 1) | u8::from(self.in1);
-        i2c.write(Self::ADDRESS, &[write_reg])?;
+        i2c.write(chip_addr, &[Self::ADDRESS, write_reg])?;
         Ok(())
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Fault {
     // When written to 1, clears the fault status bits
     pub clear: bool,
     // If set, indicates the fault was caused by an extended current limit even
-    i_limit: bool,
+    pub i_limit: bool,
     // If set, indicates that the fault was caused by an overtemperature (OTS) condition
-    ots: bool,
-    // If set, indicates the fault was caused by an undervoltage lockou
-    uvlo: bool,
+    pub ots: bool,
+    // If set, indicates the fault was caused by an undervoltage lockout
+    pub uvlo: bool,
     // If set, indicates the fault was caused by an overcurrent (OCP) event
-    ocp: bool,
+    pub ocp: bool,
     // Set if any fault condition exists
-    fault: bool,
+    pub fault: bool,
 }
 impl Fault {
     const ADDRESS: u8 = 0x01;
@@ -114,12 +131,12 @@ impl ReadRegister for Fault {
         })
     }
     #[cfg(feature = "embedded")]
-    fn new<T: I2c>(i2c: &mut T) -> Result<Self>
+    fn new(i2c: &mut I2c, chip_addr: u8) -> Result<Self>
     where
         Self: Sized,
     {
         let mut read_buf = [0u8; 1];
-        i2c.read(Self::ADDRESS, &mut read_buf)?;
+        i2c.write_read(chip_addr, &[Self::ADDRESS], &mut read_buf)?;
         let read_buf = read_buf[0];
         Ok(Self {
             clear: (read_buf >> 7) != 0,
@@ -145,14 +162,14 @@ impl WriteRegister for Fault {
     }
 
     #[cfg(feature = "embedded")]
-    fn write<T: I2c>(&self, i2c: &mut T) -> Result<()> {
+    fn write(&self, i2c: &mut I2c, chip_addr: u8) -> Result<()> {
         let write_buf = (u8::from(self.clear) << 7)
             | (u8::from(self.i_limit) << 4)
             | (u8::from(self.ots) << 3)
             | (u8::from(self.uvlo) << 2)
             | (u8::from(self.ocp) << 1)
             | u8::from(self.fault);
-        i2c.write(Self::ADDRESS, &[write_buf])?;
+        i2c.write(chip_addr, &[Self::ADDRESS, write_buf])?;
         Ok(())
     }
 }
